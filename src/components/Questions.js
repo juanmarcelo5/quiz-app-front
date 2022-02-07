c385af6ac957267f01b01239adb6ec5eda74c275
juanmarcelo5 <marcecinco@hotmail.com>
2022-02-07 10:05:38 -0300

    agregando replace4

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -26,7 +26,7 @@ export const Questions = ({ questions = [] }) => {
 				})
 			})
 
-		window.location.replace('http://juanmarcelo5.github.io/quiz-app-front/')
+		window.location.replace('https://juanmarcelo5.github.io/quiz-app-front/')
 	}, [isFinished])
 
 	const getAnswer = (answer, e) => {
