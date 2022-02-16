b6aa0f7f363927a1893bbc49c108a6fff11bba35
juanmarcelo5 <marcecinco@hotmail.com>
2022-02-15 22:45:40 -0300

    fixed bug avatar and added style

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -9,13 +9,13 @@ export const Quiz = () => {
 	return (
 		<div className='container p-3'>
 			<DataProvider>
-				<h1 className='text-title text-center mt-5'>Quiz App</h1>
+				<h1 className='text-title text-center mt-5 animate__animated animate__fadeIn'>Quiz App</h1>
 				<div className='row'>
 					{isRegister ? (
 						<ShowQuestion />
 					) : (
 						<>
-							<h3 className='text-title text-center mt-2 py-3'>
+							<h3 className='text-title text-center mt-2 py-3 animate__animated animate__fadeIn '>
 								Preguntas sobre la biblia
 							</h3>
 							<RegisterAvatar isAuth={setIsRegister} />
